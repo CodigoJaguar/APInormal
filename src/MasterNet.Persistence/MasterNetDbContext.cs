517c118f23e085f4e1d7f4530ee9779ba6a33d5d
Israel Santander Mendoza Bañuelos <israel.san_mendoza@hotmail.com>
2025-06-29 18:44:45 -0600

    first commit

---
+++ src/MasterNet.Persistence/MasterNetDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasterNet.Persistence
+{
+    public class MasterNetDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=LocalDatabase.db")
+                .LogTo(Console.WriteLine, 
+                       new[] {DbLoggerCategory.Database.Command.Name },
+                       Microsoft.Extensions.Logging.LogLevel.Information
+                       ).EnableSensitiveDataLogging();
+        }
+    }
+}
